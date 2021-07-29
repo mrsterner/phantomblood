fe3077fcc4c178f88edadba346a9e1232d8f05d2
mrsterner <simon@sterner.dev>
2021-07-29 14:44:33 +0200

    Anubis Sword can now block and it adds armor

--- src/main/java/mrsterner/phantomblood/mixin/PlayerEntityMixin.java
+++ src/main/java/mrsterner/phantomblood/mixin/PlayerEntityMixin.java
@@ -1,8 +1,10 @@
 package mrsterner.phantomblood.mixin;
 
+import mrsterner.phantomblood.common.item.AnubisSwordItem;
 import mrsterner.phantomblood.common.registry.PBObjects;
 import mrsterner.phantomblood.common.stand.Stand;
 import mrsterner.phantomblood.common.stand.StandUtils;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
@@ -23,6 +25,7 @@ import java.util.UUID;
 public abstract class PlayerEntityMixin extends LivingEntity {
 
     private static final EntityAttributeModifier TWENTYCB_MOVEMENT_SPEED_MODIFIER = new EntityAttributeModifier(UUID.fromString("a782c03d-af7b-4eb7-b997-dd396bfdc7a0"), "20th Century Boy modifier", -1.0, EntityAttributeModifier.Operation.MULTIPLY_BASE);
+    private static final EntityAttributeModifier ANUBIS_ARMOR_MODIFIER = new EntityAttributeModifier(UUID.fromString("f00b0b0f-8ad6-4a2f-bdf5-6c337ffee56c"), "Anubis modifier", 20.0, EntityAttributeModifier.Operation.ADDITION);
 
 
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
@@ -35,11 +38,9 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             PlayerEntity player = (PlayerEntity) (Object) this;
             EntityAttributeInstance movementSpeedAttribute = getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
             if (!movementSpeedAttribute.hasModifier(TWENTYCB_MOVEMENT_SPEED_MODIFIER) && StandUtils.isStandActive(player) && StandUtils.getStand(player) == Stand.TWENTY_CENTURY_BOY) {
-                System.out.println("Apply");
                 movementSpeedAttribute.addPersistentModifier(TWENTYCB_MOVEMENT_SPEED_MODIFIER);
             }
             if (movementSpeedAttribute.hasModifier(TWENTYCB_MOVEMENT_SPEED_MODIFIER) && !(StandUtils.isStandActive(player) && StandUtils.getStand(player) == Stand.TWENTY_CENTURY_BOY)) {
-                System.out.println("Remove");
                 movementSpeedAttribute.removeModifier(TWENTYCB_MOVEMENT_SPEED_MODIFIER);
             }
             ItemStack itemStack = new ItemStack(PBObjects.ANUBIS_SWORD);
@@ -60,6 +61,14 @@ public abstract class PlayerEntityMixin extends LivingEntity {
                 StandUtils.setStandActive(player, false);
                 StandUtils.setStandLevel(player, 0);
                 StandUtils.setStand(player, Stand.ANUBIS);
+            }
+            ItemStack itemStack3 = player.getStackInHand(Hand.MAIN_HAND);
+            EntityAttributeInstance armorAttribute = getAttributeInstance(EntityAttributes.GENERIC_ARMOR);
+
+            if (!armorAttribute.hasModifier(ANUBIS_ARMOR_MODIFIER) && itemStack3.getItem() instanceof AnubisSwordItem && player.isUsingItem()) {
+                armorAttribute.addPersistentModifier(ANUBIS_ARMOR_MODIFIER);
+            } else if(armorAttribute.hasModifier(ANUBIS_ARMOR_MODIFIER) && !player.isUsingItem()){
+                armorAttribute.removeModifier(ANUBIS_ARMOR_MODIFIER);
             }
         }
 
